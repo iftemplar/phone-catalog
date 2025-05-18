d42bdfa226812db35a1f3531267708c586001b9a
iftemplar <samila.mykola@gmail.com>
2025-05-18 16:13:21 +0200

    Added Vercel Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { CartProvider } from "@/context/CartContext";
 import Header from "@/components/Header/Header";
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,6 +31,7 @@ export default function RootLayout({
         <CartProvider>
           <Header />
           {children}
+          <Analytics />
         </CartProvider>
       </body>
     </html>
